5a2f85a130a2772b8a6be2a3acabe643ea90cafb
naimulislammunna <naimulislammunna742@gmail.com>
2024-12-13 00:52:57 +0600

    add

--- app/ui/dashboard/page.tsx
+++ app/ui/dashboard/page.tsx
@@ -5,8 +5,8 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchLatestInvoices, fetchRevenue } from '@/app/lib/data';
  
 export default async function Page() {
-  // const revenue = await fetchRevenue();
-  // const invoice = await fetchLatestInvoices();
+  const revenue = await fetchRevenue();
+  const invoice = await fetchLatestInvoices();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -23,8 +23,8 @@ export default async function Page() {
         /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <RevenueChart revenue={revenue}  /> */}
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <RevenueChart revenue={revenue}  />
+        <LatestInvoices latestInvoices={invoice} />
       </div>
     </main>
   );
